82885b6f2774e66eefeb3ae2e5a76cce0d861450
Arthur Axiotes <axiotesarthur@gmail.com>
2024-05-13 21:59:27 -0300

    feat: layout do filter

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,17 +1,30 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 import { AngularMaterialModule } from "../angular-material/angular-material.module";
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
     declarations: [
-    UserDetailsComponent
+    UserDetailsComponent,
+    FilterComponent
   ],
     imports: [
-        AngularMaterialModule
+        AngularMaterialModule,
+        CommonModule,
+        FormsModule
     ],
     exports: [
-      UserDetailsComponent
+      UserDetailsComponent,
+      FilterComponent,
+      CommonModule,
+      FormsModule
+    ],
+    providers: [
+      provideNativeDateAdapter()
     ]
 })
 export class ComponentsModule {  }
