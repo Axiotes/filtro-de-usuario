671c44adf63fbf6970701a1fbb1fd302543ab123
Arthur Axiotes <axiotesarthur@gmail.com>
2024-07-03 18:51:28 -0300

    feat: filtrar lista pelo nome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IUser } from "./interfaces/user.interface";
 import { UsersList } from "./data/uses-list";
 import { FilterOptions } from './interfaces/filter-options.interface';
@@ -8,10 +8,18 @@ import { FilterOptions } from './interfaces/filter-options.interface';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  userList: IUser[] = []
+export class AppComponent implements OnInit {
+  userList: IUser[] = [];
+  userListFiltred: IUser[] = [];
   userSelected: IUser = {} as IUser;
   showUserDetails: boolean = false;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.userList = UsersList;
+      this.userListFiltred = UsersList;
+    });
+  }
 
   public receiveIndexUser(index: number){
     this.userSelected = UsersList[index];
@@ -20,5 +28,27 @@ export class AppComponent {
 
   public receiveFilter(filterOptions: FilterOptions){
     console.log(filterOptions);
+
+    this.userListFiltred = this.filterUserList(filterOptions, this.userList);
+  }
+
+  public filterUserListByName(name: string | undefined, userList: IUser[]): IUser[] {
+    const NAME_NOT_TYPPED = name === undefined;
+
+    if (NAME_NOT_TYPPED) {
+      return UsersList;
+    }
+
+    const filteredList = userList.filter((user: IUser) => user.nome.toLocaleLowerCase().includes(name.toLocaleLowerCase()));
+
+    return filteredList;
+  }
+
+  public filterUserList(filterOptions: FilterOptions, userList: IUser[]): IUser[] {
+    let filteredList: IUser[] = [];
+
+    filteredList = this.filterUserListByName(filterOptions.name, UsersList);
+
+    return filteredList;
   }
 }
